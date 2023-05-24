16a5d17550fbeaa72bb958ccd07f50c4ef9d1305
mugilaananthan <89967104+Mugil1410@users.noreply.github.com>
2023-05-24 10:50:54 +0530

    Add files via upload

---
+++ c2tc/src/day18/TestGenerics1.java
@@ -0,0 +1,22 @@
+package day18;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class TestGenerics1 {
+	public static void main(String args[]){  
+		ArrayList<String> list=new ArrayList<String>();  
+		list.add("hello ");  
+		list.add("world ");  
+		//list.add(32);//compile time error  
+		  
+		String s=list.get(1);//type casting is not required  
+		System.out.println("element is: "+s);  
+		  
+		Iterator<String> itr=list.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		}
+
+}
